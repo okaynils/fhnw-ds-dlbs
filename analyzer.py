34e1be2565aa9f31b12ac11e06a682a15e0f65bb
okaynils <nils.fahrni@students.fhnw.ch>
2024-12-08 21:19:37 +0100

    added analyzer class

--- analyzer.py
+++ analyzer.py
@@ -2,12 +2,13 @@ import os
 
 import wandb
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import torch
 
 MODELS_PATH = "models"
 
 class Analyzer:
-    def __init__(self, run_id, project_name, entity_name, test_dataset):
+    def __init__(self, run_id, project_name, entity_name, test_dataset, model, device="cpu"):
         """
         Initializes the Analyzer object with run details.
 
@@ -21,24 +22,59 @@ class Analyzer:
         self.entity_name = entity_name
         self.history = None
         self.test_dataset = test_dataset
+        self.device = device
+        self.model = model.to(device)
 
     def _load_model(self):
         models = os.listdir(MODELS_PATH)
         
         for model in models:
             if self.run_id in model:
-                model = torch.load(os.path.join(MODELS_PATH, model))
-                return model
+                model_state = torch.load(os.path.join(MODELS_PATH, model))
+                
+                self.model.load_state_dict(model_state)
+                
+                return self.model
 
     def _test_n_samples(self, test_sample_ids: list = None):
         model = self._load_model()
-        
-        samples = self.test_dataset[test_sample_ids]
-        
-        for image, label in samples:
+        for idx in test_sample_ids:
+            image, label, scene = self.test_dataset[idx]
+            
             print(f"Image shape: {image.shape}")
             print(f"Label shape: {label.shape}")
-        
+            print(f"Scene: {scene}")
+            
+            model.eval()
+            with torch.no_grad():
+                image = image.unsqueeze(0).to(self.device)
+                output = model(image)
+                pred = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
+                
+                cmap = plt.get_cmap('tab20')
+                unique_classes = list(range(19))  # Adjust if there are fewer/more classes
+                legend_patches = [mpatches.Patch(color=cmap(i / len(unique_classes)), label=f'Class {i}') for i in unique_classes]
+
+                print(pred, label)
+
+                plt.figure(figsize=(12, 6))
+
+                # Ground Truth subplot
+                plt.subplot(1, 2, 1)
+                plt.imshow(label, cmap='tab20')
+                plt.title("Ground Truth")
+                plt.axis('off')
+                plt.legend(handles=legend_patches, loc='upper right', bbox_to_anchor=(1.2, 1.0), title="Classes")
+
+                # Prediction subplot
+                plt.subplot(1, 2, 2)
+                plt.imshow(pred, cmap='tab20')
+                plt.title("Prediction")
+                plt.axis('off')
+                plt.legend(handles=legend_patches, loc='upper right', bbox_to_anchor=(1.2, 1.0), title="Classes")
+
+                plt.tight_layout()
+                plt.show()
 
     def _fetch_data(self):
         """
